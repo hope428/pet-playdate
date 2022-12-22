35551a38929ae77a0c69de96b3e372f58aa50ed0
Brian Chapman <chapman.brian86@gmail.com>
2022-12-22 15:43:07 -0500

    created a npm script to run seeds and created an env file for my Mysql login

---
+++ seeds/seed.js
@@ -0,0 +1,6 @@
+const sequelize = require("../config/connection");
+const { Pet, PetPlayDate, PlayDate, User } = require("../models");
+
+const petSeedData = require("../data/petSeedData.json");
+
+ 
