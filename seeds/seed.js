e32575b1e83a5a8a2ffc9bdeb810054aff9fba28
Brian Chapman <chapman.brian86@gmail.com>
2022-12-22 15:57:37 -0500

    Created JSON files for other seed data and started writing index.js for seeds folder

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,5 +2,10 @@ const sequelize = require("../config/connection");
 const { Pet, PetPlayDate, PlayDate, User } = require("../models");
 
 const petSeedData = require("../data/petSeedData.json");
+const playDateSeedData = require("../data/playDateSeedData.json");
+const userSeedData = require("../data/userSeedData.json");
 
+const seedDatabase = async () => {
+await sequelize.sync({ force: true });
+};
  
