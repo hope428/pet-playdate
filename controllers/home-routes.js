5b1c13c0768dfc57fda7d87c74e22554c66f0f18
Leon Forsythe <lvforsythe95@gmail.com>
2022-12-27 13:48:11 -0500

    added post route for creating playdate

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/home', (req, res) => {
-    res.render('homepage')
-})
+router.get("/home", (req, res) => {
+  res.render("homepage");
+});
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard')
-})
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
 
 module.exports = router;
