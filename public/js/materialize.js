e14aea99c10df63a5895dc47218d0b28fd6a3a2f
Brian Chapman <chapman.brian86@gmail.com>
2023-01-03 14:27:46 -0500

    Adding logic for Login button on homepage and restructured signup.JS

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -8,3 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 });
+
+// Login button on homepage
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
