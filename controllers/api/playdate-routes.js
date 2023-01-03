3984ef46b8d55c84e0f2c53d7224df4d2eef2801
Leon Forsythe <lvforsythe95@gmail.com>
2023-01-03 13:14:25 -0500

    connected petsand events through creating new pet playdate

--- controllers/api/playdate-routes.js
+++ controllers/api/playdate-routes.js
@@ -1,57 +1,61 @@
-const router = require('express').Router();
-const {Playdate, PetPlaydate, Pet} = require('../../models')
+const router = require("express").Router();
+const { Playdate, PetPlaydate, Pet } = require("../../models");
 
 // localhost:3001/api/playdates/new-playdate
 router.post("/new-playdate", async (req, res) => {
-    try {
-      const playdate = await Playdate.findOne({
-        where: {
-          location: req.body.location,
-          date: req.body.date
-        }
-      })
+  try {
+    const playdate = await Playdate.findOne({
+      where: {
+        location: req.body.location,
+        date: req.body.date,
+      },
+    });
 
-      if(!playdate){
-        const newPlaydate = await Playdate.create({
-          location: req.body.location,
-          date: req.body.date,
-          pet_id: req.body.pet_id
-        });
+    if (!playdate) {
+      const newPlaydate = await Playdate.create({
+        location: req.body.location,
+        date: req.body.date,
+      });
 
-        const newPetPlaydate = await PetPlaydate.create({
-          // pet id can be tied to current pet's button with data-value
-          pet_id: req.body.pet_id,
-          playdate_id: newPlaydate.id
-        })
-        res.json("created new playdate")
-        return;
-      }
-      // updates through table to reflect pets added to playdate
-      const updatePetPlaydate = await PetPlaydate.findOne({
-        where: {
-          playdate_id: playdate.id,
-        }
-      })
-      await newPetPlaydate.update({
-        
-      })
-  
-      res.status(201).json("updating playdate");
-    } catch (error) {
-      res.status(500).json(error);
+      const newPetPlaydate = await PetPlaydate.create({
+        // pet id can be tied to current pet's button with data-value
+        pet_id: req.body.pet_id,
+        playdate_id: newPlaydate.id,
+      });
+      res.json("created new playdate");
+      return;
     }
-  });
+    console.log(playdate.id);
+    console.log(req.body.pet_id);
+    // updates through table to reflect pets added to playdate
+    const updatePetPlaydate = await PetPlaydate.create({
+      playdate_id: playdate.id,
+      pet_id: req.body.pet_id,
+    });
 
-  //this route gets back all playdates and pets associated with that playdate
-  router.get("/", async(req, res) => {
-    try {
-      const playdates = await Pet.findAll({
-        include: {model: Playdate, through: PetPlaydate, as: 'pet_playdates'}
-      })
-      res.json(playdates)
-    } catch (error) {
-      
-    }
-  })
+    res.status(201).json(updatePetPlaydate);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  module.exports = router
+//this route gets back all playdates and pets associated with that playdate
+router.get("/", async (req, res) => {
+  try {
+    const playdates = await Pet.findAll({
+      include: { model: Playdate, through: PetPlaydate, as: "pet_playdates" },
+    });
+    res.json(playdates);
+  } catch (error) {}
+});
+
+router.get('/all', async(req, res) => {
+  try {
+    const playdates = await Playdate.findAll({
+      include: { model: Pet, through: PetPlaydate, as: "playdate_pets" },
+    });
+    res.json(playdates);
+  } catch (error) {}
+})
+
+module.exports = router;
