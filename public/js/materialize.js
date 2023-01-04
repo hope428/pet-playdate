88b5afebe5f82518f1439e8a10a79957e4d95157
D <Dickerson.Morgan@outlook.com>
2023-01-04 16:09:21 -0500

    highlighted todo areas

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -29,3 +29,11 @@ loginClick = (event) => {
     }
   });
 };
+const file = document.querySelector("#file")
+file.addEventListener("change", function() {
+  const reader = new FileReader()
+  reader.addEventListener("load", () => {
+    document.querySelector("#image").src = reader.result
+  })
+  reader.readAsDataURL(this.files[0]);
+})
