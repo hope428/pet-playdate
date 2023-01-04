11fbaee0015b24ba7f43338034cb3232d628f725
D <Dickerson.Morgan@outlook.com>
2023-01-04 11:23:54 -0500

    redid the navbar. Also fixed minor styling issues

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -9,11 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
 });
-
 // Login button on homepage
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
+  });
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
   });
